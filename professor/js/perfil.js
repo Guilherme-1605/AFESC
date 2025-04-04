dbfde28a55df9707307b28e74b3d8a8fdd2d8eb9
Guilherme-1605 <guilhermepenna155@gmail.com>
2025-04-04 14:03:47 -0300

    perfil do professor

---
+++ professor/js/perfil.js
@@ -0,0 +1,7 @@
+    // Script para alternar a visibilidade do menu lateral
+    const menuToggle = document.getElementById("menuToggle");
+    const menuLateral = document.getElementById("menuLateral");
+
+    menuToggle.addEventListener("click", () => {
+        menuLateral.classList.toggle("aberto");
+    });
